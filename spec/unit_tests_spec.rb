d9fbadebb3d667441e52560aab9a91a5cf623cc7
Joanna Brigham <joannabrigham@hotmail.co.uk>
2021-02-02 18:23:29 +0100

    second test

--- spec/unit_tests_spec.rb
+++ spec/unit_tests_spec.rb
@@ -4,4 +4,9 @@ describe 'DockingStation' do
   it 'check if Dockingstation exists' do
     expect(DockingStation.new.is_a?(DockingStation)).to eq true
   end
-end
+
+  #it {to respond_to(release_bike)}
+  it 'check if responds to release_bike' do
+    expect(DockingStation.new.respond_to?(:release_bike))
+  end
+  end
