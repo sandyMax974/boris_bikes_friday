b2c9d3be6432e33f23b4b22d7e6a039ec12ed14a
Peter Allen <peter-james-allen@hotmail.com>
2021-02-03 16:46:57 +0000

    docking error test

--- lib/Docking_station.rb
+++ lib/Docking_station.rb
@@ -1,23 +1,27 @@
 class DockingStation
-	attr_reader :bike_list 
-	
+	attr_reader :bike_list
+
 	def initialize
-		@bike_list = ["bike", "another bike"]
+		@bike_list = []
 	end
 
 	def release_bike
-		Bike.new
+    if @bike_list.length == 0
+      raise "No bikes avaliable"
+    else
+		  Bike.new
+    end
 	end
 
 	def dock(bike)
 		return "Bike Docked!" if bike.is_a? Bike
 	end
 
-	def check_bike 
+	def check_bike
 		if @bike_list.length > 0
 			return true
 		else return false
-		end 
+		end
 	end
 
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -5,7 +5,7 @@ describe DockingStation do
 	it { is_expected.to respond_to(:check_bike)}
 
   docking_station = DockingStation.new
-  bike = Bike.new 
+  bike = Bike.new
 
 	it 'relase a bike' do
 		bike = docking_station.release_bike
@@ -13,17 +13,21 @@ describe DockingStation do
   end
 
   it 'check if its working' do
-    expect(bike.working?).to eq true
+    expect{bike.working?}.to eq true
 	end
-	
+
 	# dock is a method held by DockingStation, taking an argument of (bike)
 
 	it 'can dock a bike' do
 		expect(docking_station.dock(bike)).to eq "Bike Docked!"
 	end
- 
-	it 'should return true if a bike is available' do 
-		expect(docking_station.check_bike).to be true 
+
+	it 'should return true if a bike is available' do
+    docking_station.dock(bike)
+		expect(docking_station.check_bike).to be true
 	end
 
+  it 'return error if release method called on empty station' do
+    expect { docking_station.release_bike }.to raise_error("No bikes avaliable")
+  end
 end
