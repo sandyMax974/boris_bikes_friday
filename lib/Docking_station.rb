ca118970ed28f8868370d6314b227a2667becadf
Aniruddh <ojhaa94@gmail.com>
2021-02-03 15:49:37 +0000

    adds check_bike and bike_list

--- lib/Docking_station.rb
+++ lib/Docking_station.rb
@@ -1,8 +1,24 @@
 class DockingStation
+	attr_reader :bike_list 
+	
+	def initialize
+		@bike_list = ["bike", "another bike"]
+	end
 
-  def release_bike
-    Bike.new
-  end
+	def release_bike
+		Bike.new
+	end
+
+	def dock(bike)
+		return "Bike Docked!" if bike.is_a? Bike
+	end
+
+	def check_bike 
+		if @bike_list.length > 0
+			return true
+		else return false
+		end 
+	end
 
 end
 
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,17 +1,29 @@
 require 'Docking_station'
 
 describe DockingStation do
-  it { is_expected.to respond_to(:release_bike)}
+	it { is_expected.to respond_to(:release_bike)}
+	it { is_expected.to respond_to(:check_bike)}
 
   docking_station = DockingStation.new
-  bike = docking_station.release_bike
+  bike = Bike.new 
 
-  it 'relase a bike' do
+	it 'relase a bike' do
+		bike = docking_station.release_bike
     expect(bike).to be_instance_of Bike
   end
 
   it 'check if its working' do
     expect(bike.working?).to eq true
-  end
+	end
+	
+	# dock is a method held by DockingStation, taking an argument of (bike)
+
+	it 'can dock a bike' do
+		expect(docking_station.dock(bike)).to eq "Bike Docked!"
+	end
+ 
+	it 'should return true if a bike is available' do 
+		expect(docking_station.check_bike).to be true 
+	end
 
 end
